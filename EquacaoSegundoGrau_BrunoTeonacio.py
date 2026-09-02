7271c6c566e864ebb6b784a1bf5c98958e2b9b32
BRUNO TEONACIO DOS SANTOS <20262014050001@ifrn.local>
2026-09-01 21:11:31 -0300

    first commit

---
+++ EquacaoSegundoGrau_BrunoTeonacio.py
@@ -0,0 +1,22 @@
+import math # Para usar a raiz quadrada
+
+valores = input("Digite os valores de a, b e c, de uma equação do segundo grau (separados por espaço):").split()
+a = int(valores[0])
+b = int(valores[1])
+c = int(valores[2])
+delta = 0
+x1 = 0
+x2 = 0
+
+if a == 0:
+    print("O valor de a não pode ser zero. Envie novamente os valores.")
+else:
+    delta = (b**2) - (4*a*c)
+
+    if delta < 0:
+        print("O valor de delta da equação, {}, é menor que 0, portanto as raízes são imaginárias.".format(delta))
+    else:    
+        x1 = (-b + math.sqrt(delta))/(2*a)
+        x2 = (-b - math.sqrt(delta))/(2*a)
+
+        print("A equação de segundo grau informada possui as raízes {} e {}.".format(x1,x2))
---
+++ SaqueTerminal_BrunoTeonacio.py
@@ -0,0 +1,30 @@
+#
+# Lógica:
+# - Uma divisão inteira (//) de um número por outro retorna o valor inteiro do resultado;
+# - Ao dividirmos o saque informado por uma das cédulas (divisão inteira) retornamos a quantidade de cédulas do tipo;
+# - O resto da divisão (%) dividimos pela cedula menor a anterior;
+# - Se chegarmos a 1, sabemos que o valor tem uma moeda de 1 real.
+#
+
+saque = int(input("Digite o valor (R$) para o saque: "))
+cedulas = [100, 50, 20, 10, 5, 2, 1] # Aqui, 1 seria a moeda de 1 real.
+valor_que_resta = saque # Para o for
+quantidade_cedula = [0, 0, 0, 0, 0, 0, 0] # Quantidades de cada cédula calculada.
+
+if (saque <= 0):
+    print("Informe um saque válido.")
+else:
+
+    print("Quantidade de cédulas/moedas do saque de valor R$ {}:".format(saque))
+
+    for count, cedula in enumerate(cedulas):
+    
+        # Divide o valor restante do saque pela cedula (EX: 200/100 = 2 cedulas de 100)
+        quantidade_cedula[count] = valor_que_resta // cedula
+        # Guarda o valor que resta para a próxima divisão inteira
+        valor_que_resta = valor_que_resta % cedula
+
+    for count, quant in enumerate(quantidade_cedula):
+    
+        if quant > 0:
+            print("Quantidade de cedulas/moedas de R$ {} igual a {}.".format(cedulas[count],quant))
