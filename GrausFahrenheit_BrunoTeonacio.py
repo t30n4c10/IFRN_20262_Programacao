4344396bab6351b086cbb35d297771a4b8d94092
BRUNO TEONACIO DOS SANTOS <20262014050001@ifrn.local>
2026-09-01 21:13:36 -0300

    Fahrenheit

---
+++ GrausFahrenheit_BrunoTeonacio.py
@@ -0,0 +1,3 @@
+grausCelcius = int(input("Digite uma temperatura em Graus Celcius: "))
+grausFahrenheit = ((9*grausCelcius) + 160)/5
+grausCelcius = input("O valor informado em Celcius, {} graus, equivale a {} graus em Fahrenheit.".format(grausCelcius,grausFahrenheit))
