b10d68f13fb1cdb7d4d1de054a5c38935d684aea
BRUNO TEONACIO DOS SANTOS <20262014050001@ifrn.local>
2026-09-01 21:36:09 -0300

    Atualizacao_SegundoGrau

--- EquacaoSegundoGrau_BrunoTeonacio.py
+++ EquacaoSegundoGrau_BrunoTeonacio.py
@@ -1,4 +1,4 @@
-import math # Para usar a raiz quadrada
+#import math # Para usar a raiz quadrada
 
 valores = input("Digite os valores de a, b e c, de uma equação do segundo grau (separados por espaço):").split()
 a = int(valores[0])
@@ -16,7 +16,10 @@ else:
     if delta < 0:
         print("O valor de delta da equação, {}, é menor que 0, portanto as raízes são imaginárias.".format(delta))
     else:    
-        x1 = (-b + math.sqrt(delta))/(2*a)
-        x2 = (-b - math.sqrt(delta))/(2*a)
+        #x1 = (-b + math.sqrt(delta))/(2*a)
+        #x2 = (-b - math.sqrt(delta))/(2*a)
+        x1 = (-b + delta**0.5)/(2*a)
+        x2 = (-b - delta**0.5)/(2*a)
+
 
         print("A equação de segundo grau informada possui as raízes {} e {}.".format(x1,x2))
