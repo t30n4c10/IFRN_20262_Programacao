1412f606926965d60c96c3ac5cbc155ad31102f4
Bruno Teonácio <teonacio.bruno@gmail.com>
2026-09-04 20:45:47 -0300

    Correções IRPF

--- ImpostoRenda_BrunoTeonacio.py
+++ ImpostoRenda_BrunoTeonacio.py
@@ -5,13 +5,13 @@ IRPF = 0
 if salario <= 2428.80:
     print("Não precisa pagar imposto de renda.")
 elif 2428.81 <= salario <= 2826.65:
-    IRPF = salario*1.075 - 182.16
+    IRPF = salario*0.075 - 182.16
 elif 2826.66 <= salario <= 3751.05:
-    IRPF = salario*1.15 - 394.16
+    IRPF = salario*0.15 - 394.16
 elif 3751.06 <= salario <= 4664.68:
-    IRPF = salario*1.225 - 675.49
+    IRPF = salario*0.225 - 675.49
 elif salario > 4664.68:
-    IRPF = salario*1.275 - 908.73
+    IRPF = salario*0.275 - 908.73
     
 # Segundo cálculo
 if salario <= 5000:
@@ -22,6 +22,9 @@ if salario <= 5000:
 elif 5000.01 <= salario <= 7350:
     
     # Redução progressiva, de acordo com o salário
-    IRPF = IRPF - 978.62 - (0.133145*salario)
-    
-print("Seu IRPF a pagar será {}."format(IRPF))
+    IRPF = 978.62 - (0.133145*salario)
+
+if IRPF > 0:
+    print("Seu IRPF a pagar será {}.".format(IRPF))
+else:
+    print("Não precisa pagar imposto de renda.")
