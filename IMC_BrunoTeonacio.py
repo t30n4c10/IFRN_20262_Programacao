c0b6e1a5ed7a73649bbd19cdea337862adf29aaa
Bruno Teonácio <teonacio.bruno@gmail.com>
2026-09-04 21:06:25 -0300

    Atualização IMC

--- IMC_BrunoTeonacio.py
+++ IMC_BrunoTeonacio.py
@@ -4,15 +4,15 @@ altura = float(input("Digite a sua altura: "))
 
 IMC = peso/(altura**2)
 
-if IMC < 18.5:
+if IMC < 18.500:
     print("Você está com baixo peso.")
-elif 18.5 <= IMC < 24.9:
+elif 18.500 <= IMC < 24.900:
     print("Você está com peso normal.")
-elif 24.9 <= IMC < 25.9:
+elif 24.900 <= IMC < 25.900:
     print("Você está com sobrepeso.")
-elif 25.9 <= IMC < 34.9:
+elif 25.900 <= IMC < 34.900:
     print("Você está com sobrepeso grau I")
-elif 34.9 <= IMC < 39.9:
+elif 34.900 <= IMC < 39.900:
     print("Você está com sobrepeso grau II")
-elif IMC >= 39.9:
+elif IMC >= 39.900:
     print("Você está com sobrepeso grau III")
