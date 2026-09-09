fe3a5324e4a0274f986ada1cde86a6ade1110193
BRUNO TEONACIO DOS SANTOS <20262014050001@ifrn.local>
2026-09-08 21:22:35 -0300

    Primo

---
+++ Primo_BrunoTeonacio.py
@@ -0,0 +1,14 @@
+numero = int(input("Digite um valor: "))
+cont = 0
+num_div = 0
+
+while cont <= numero:
+    cont = cont + 1
+    
+    if numero % cont == 0:
+        num_div = num_div + 1
+        
+if num_div == 2:
+    print("É primo!!!")
+else:
+    print("Não é primo... :(")
