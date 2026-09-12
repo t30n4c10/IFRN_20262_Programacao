fea9bf81a1d58b11b288f249ca2d87bf5fd79cca
Bruno Teonácio <teonacio.bruno@gmail.com>
2026-09-12 16:04:40 -0300

    Estudo 12-09

--- SomaPrimos2milhoes_BrunoTeonacio.py
+++ SomaPrimos2milhoes_BrunoTeonacio.py
@@ -1,20 +1,22 @@
 # https://projecteuler.net/problem=10
-# Código não otimizado
+# Código não otimizado (por isso, com limite = 2000000, irá demorar muito pra executar)
+# Resposta: 142913828922
 
 soma_primos = 0
-limite = 100
-cont1 = 2 # Primeiro while
-cont2 = 0 # Segundo while
+limite = 2000000
+cont1 = 2 # Começa no primeiro número primo.
 
 while cont1 < limite:
 
-    #print("cont1 = {}".format(cont1))
     num_div = 0
+    cont2 = 1 # Para cada novo número que é verificado se é primo, esse contador precisa ser resetado.
 
-    while cont2 < limite:
-        #print(cont2)
-        if cont2 != 0 and cont1 % cont2 == 0:
+    # O segundo while (abaixo) verifica se o número do primeiro while é primo.
+    while cont2 <= cont1:
+
+        if cont1 % cont2 == 0:
             num_div = num_div + 1
+
         cont2 += 1
 
     if num_div == 2:
@@ -22,4 +24,4 @@ while cont1 < limite:
 
     cont1 += 1
 
-print(soma_primos)
+print("A soma de todos os números primos abaixo de {} é: {}.".format(limite,soma_primos))
