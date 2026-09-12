8b93e5ae4ebc6d5e00109f2f34792c5db2782bf7
BRUNO TEONACIO DOS SANTOS <20262014050001@ifrn.local>
2026-09-11 21:28:41 -0300

    Aula Programação 11-09

--- DiferencaQuadrados_BrunoTeonacio.py
+++ DiferencaQuadrados_BrunoTeonacio.py
@@ -1,25 +1,27 @@
+# https://projecteuler.net/problem=6
+# Código não otimizado
+
 # (1^2 + 2^2 + ... + 1000^2)
-soma_do_quadrado = 1
+soma_do_quadrado = 0
 
 # (1 + 2 + ... + 1000)^2
-quadrado_da_soma = 1
+quadrado_da_soma = 0
 
 count = 1
+limite = 100
 
 # Soma do quadrado
-while count <= 1000:
-    soma_do_quadrado = soma_do_quadrado + count^2
+while count <= limite:
+    soma_do_quadrado = soma_do_quadrado + count**2
     count += 1
 
 count = 1
 
 # Quadrado da soma
-while count <= 1000:
+while count <= limite:
     quadrado_da_soma = quadrado_da_soma + count
     count += 1
-quadrado_da_soma = quadrado_da_soma**2
+quadrado_da_soma = quadrado_da_soma**2 # Note que essa linha está fora do while.
 
-print(soma_do_quadrado)
-print(quadrado_da_soma)
-
-print("A diferença entre a soma dos quadrados (valor = {}) e o quadrado da soma (valor = {}) dos 1000 primeiros numeros é = {}".format(soma_do_quadrado,quadrado_da_soma,quadrado_da_soma - soma_do_quadrado))
+# Quadrado da soma é SEMPRE maior que a soma dos quadrados
+print("A diferença entre a soma dos quadrados (valor = {}) e o quadrado da soma (valor = {}) dos {} primeiros numeros é = {}".format(soma_do_quadrado,quadrado_da_soma,limite,quadrado_da_soma - soma_do_quadrado))
