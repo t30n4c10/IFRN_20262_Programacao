6a1c45733770ba15bb3355c31a86a20e9fe70a6a
BRUNO TEONACIO DOS SANTOS <20262014050001@ifrn.local>
2026-09-22 21:58:07 -0300

    Atualização Número Pitagórico

--- NumeroPitagorico_BrunoTeonacio.py
+++ NumeroPitagorico_BrunoTeonacio.py
@@ -1,11 +1,9 @@
-# Encontre um trio de numeros pitagoricos, 'a' 'b' e 'c', tal que a + b + c = 1000
-# Range de a -> 1 a 1000
-# Range de b -> 1+1 (um valor a frente) até 1000
+# https://projecteuler.net/problem=9
 # Código não otimizado.
 
 for a in range(1, 1001):
     for b in range(a + 1, 1001):
         c = 1000 - a - b
 
-        if a**2 + b**2 == c**2:
-            print("a = {}, b = {}, c = {}".format(a,b,c))
+        if (c > b) and (a**2 + b**2 == c**2):
+            print("a = {}, b = {}, c = {}, a*b*c = {}".format(a,b,c,a*b*c))
